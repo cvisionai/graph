5a73d8bd86744a6c6c58a7e7d9db79f25f163c9a
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2018-12-28 14:31:20 -0500

    Add GAEC to module

--- bindings/graph.cxx
+++ bindings/graph.cxx
@@ -1,5 +1,27 @@
 #include <boost/python.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+#include "andres/graph/graph.hxx"
+#include "andres/graph/multicut/greedy-additive.hxx"
+
+using namespace boost::python;
+using namespace andres::graph;
 
 BOOST_PYTHON_MODULE(graph) {
-    
+    class_<std::vector<double> >("DoubleVec")
+        .def(vector_indexing_suite<std::vector<double> >())
+    ;
+    class_<std::vector<long> >("LongVec")
+        .def(vector_indexing_suite<std::vector<long> >())
+    ;
+    class_<Graph<>>("Graph")
+        .def("insertVertices", &Graph<>::insertVertices)
+        .def("insertEdge", &Graph<>::insertEdge)
+    ;
+    def("greedyAdditiveEdgeContraction",
+        &multicut::greedyAdditiveEdgeContraction<
+            Graph<>,
+            std::vector<double>,
+            std::vector<long> 
+        >
+    );
 }
