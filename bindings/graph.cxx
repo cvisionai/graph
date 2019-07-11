d4c31e11ea4a1a7105bc1df4774d9d3bed4df2dd
Brian Tate <brian.tate@cvisionai.com>
2019-07-11 13:54:12 -0400

    keep using doubles for now

--- bindings/graph.cxx
+++ bindings/graph.cxx
@@ -31,14 +31,14 @@ BOOST_PYTHON_MODULE(graph) {
     def("greedyAdditiveEdgeContraction",
         &multicut::greedyAdditiveEdgeContraction<
             Graph<>,
-            std::vector<float>,
+            std::vector<double>,
             std::vector<long> 
         >
     );
     def("constrainedGreedyAdditiveEdgeContraction",
         &multicut::constrainedGreedyAdditiveEdgeContraction<
             Graph<>,
-            std::vector<float>,
+            std::vector<double>,
             std::vector<std::pair<long, long> >,
             std::vector<long> 
         >
